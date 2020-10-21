e5d649b5cc70a017472a97d5ad01000eb2d95236
Justin Oakes <justin.t.oakes@gmail.com>
2020-10-21 15:22:50 -0500

    Initial Commit

---
+++ Key Bound/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Key Bound
+//
+//  Created by Justin Oakes on 10/21/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
