7faf863862c9a69916d69a28c95acf1c4ad5430b
Justin Oakes <justin.t.oakes@gmail.com>
2020-10-22 14:08:42 -0500

    working for sound and brightness

--- Key Bound/AppDelegate.swift
+++ Key Bound/AppDelegate.swift
@@ -22,5 +22,102 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
 
+    @IBAction func volUp(_ sender: Any) {
+        let keyCode = NX_KEYTYPE_SOUND_UP
+        let keyDownEvent = NSEvent.otherEvent(with: .systemDefined,
+                                       location: NSPoint.zero,
+                                       modifierFlags: NSEvent.ModifierFlags(rawValue: 0xa00),
+                                       timestamp: 0,
+                                       windowNumber: 0,
+                                       context: nil,
+                                       subtype: 8,
+                                       data1: (Int((keyCode << 16 as Int32) | (0xa << 8 as Int32))),
+                                       data2: -1)
+        keyDownEvent?.cgEvent?.post(tap: .cghidEventTap)
+        let keyUpEvent = NSEvent.otherEvent(with: .systemDefined,
+                                            location: NSPoint.zero,
+                                            modifierFlags: NSEvent.ModifierFlags(rawValue: 0xb00),
+                                            timestamp: 0,
+                                            windowNumber: 0,
+                                            context: nil,
+                                            subtype: 8,
+                                            data1: (Int((keyCode << 16 as Int32) | (0xb << 8 as Int32))),
+                                            data2: -1)
+        keyUpEvent?.cgEvent?.post(tap: .cghidEventTap)
+        
+    }
+    
+    @IBAction func volDown(_ sender: Any) {
+        let keyCode = NX_KEYTYPE_SOUND_DOWN
+        let keyDownEvent = NSEvent.otherEvent(with: .systemDefined,
+                                       location: NSPoint.zero,
+                                       modifierFlags: NSEvent.ModifierFlags(rawValue: 0xa00),
+                                       timestamp: 0,
+                                       windowNumber: 0,
+                                       context: nil,
+                                       subtype: 8,
+                                       data1: (Int((keyCode << 16 as Int32) | (0xa << 8 as Int32))),
+                                       data2: -1)
+        let keyUpEvent = NSEvent.otherEvent(with: .systemDefined,
+                                            location: NSPoint.zero,
+                                            modifierFlags: NSEvent.ModifierFlags(rawValue: 0xb00),
+                                            timestamp: 0,
+                                            windowNumber: 0,
+                                            context: nil,
+                                            subtype: 8,
+                                            data1: (Int((keyCode << 16 as Int32) | (0xb << 8 as Int32))),
+                                            data2: -1)
+        keyDownEvent?.cgEvent?.post(tap: .cghidEventTap)
+        keyUpEvent?.cgEvent?.post(tap: .cghidEventTap)
+    }
+    
+    @IBAction func brightUp(_ sender: Any) {
+        let keyCode = NX_KEYTYPE_BRIGHTNESS_UP
+        let keyDownEvent = NSEvent.otherEvent(with: .systemDefined,
+                                       location: NSPoint.zero,
+                                       modifierFlags: NSEvent.ModifierFlags(rawValue: 0xa00),
+                                       timestamp: 0,
+                                       windowNumber: 0,
+                                       context: nil,
+                                       subtype: 8,
+                                       data1: (Int((keyCode << 16 as Int32) | (0xa << 8 as Int32))),
+                                       data2: -1)
+        keyDownEvent?.cgEvent?.post(tap: .cghidEventTap)
+        let keyUpEvent = NSEvent.otherEvent(with: .systemDefined,
+                                            location: NSPoint.zero,
+                                            modifierFlags: NSEvent.ModifierFlags(rawValue: 0xb00),
+                                            timestamp: 0,
+                                            windowNumber: 0,
+                                            context: nil,
+                                            subtype: 8,
+                                            data1: (Int((keyCode << 16 as Int32) | (0xb << 8 as Int32))),
+                                            data2: -1)
+        keyUpEvent?.cgEvent?.post(tap: .cghidEventTap)
+    }
+    
+    
+    @IBAction func brightDown(_ sender: Any) {
+        let keyCode = NX_KEYTYPE_BRIGHTNESS_DOWN
+        let keyDownEvent = NSEvent.otherEvent(with: .systemDefined,
+                                       location: NSPoint.zero,
+                                       modifierFlags: NSEvent.ModifierFlags(rawValue: 0xa00),
+                                       timestamp: 0,
+                                       windowNumber: 0,
+                                       context: nil,
+                                       subtype: 8,
+                                       data1: (Int((keyCode << 16 as Int32) | (0xa << 8 as Int32))),
+                                       data2: -1)
+        keyDownEvent?.cgEvent?.post(tap: .cghidEventTap)
+        let keyUpEvent = NSEvent.otherEvent(with: .systemDefined,
+                                            location: NSPoint.zero,
+                                            modifierFlags: NSEvent.ModifierFlags(rawValue: 0xb00),
+                                            timestamp: 0,
+                                            windowNumber: 0,
+                                            context: nil,
+                                            subtype: 8,
+                                            data1: (Int((keyCode << 16 as Int32) | (0xb << 8 as Int32))),
+                                            data2: -1)
+        keyUpEvent?.cgEvent?.post(tap: .cghidEventTap)
+    }
 }
 
